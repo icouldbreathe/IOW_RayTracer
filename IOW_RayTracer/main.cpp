580418ed5a2881c7709e0e08ac0afaf464ad54ed
icouldbreathe <ciomenas@icloud.com>
2022-05-11 14:30:29 +0100

    output image gradient of 4 colours scaled by image size

--- IOW_RayTracer/main.cpp
+++ IOW_RayTracer/main.cpp
@@ -2,13 +2,31 @@
 
 int main()
 {
-    const int imageWidth = 3;
-    const int imageHeight = 2;
+    const int imageWidth = 512;
+    const int imageHeight = 512;
     const int maxColor = 255;
 
     std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n" << maxColor << "\n";
-    std::cout << "255 0 0 0 255 0 0 0 255\n255 255 0 255 255 255 0 0 0\n";
+    
+    for (int i = imageHeight-1; i >= 0; --i)
+    {
+        for (int j = 0; j < imageWidth; ++j)
+        {
+            // Scales the rgb values across the image
+            // Red from left to right 0-255, Green from top to bottom 255-0
+            // Blue tint of 0.25
+            auto r = double(j) / (imageWidth-1);
+            auto g = double(i) / (imageHeight-1);
+            auto b = 0.25;
 
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
+    }
+    
     return 0;
 }
 
