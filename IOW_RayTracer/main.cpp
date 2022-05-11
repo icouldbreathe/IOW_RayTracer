159ff66a3916761b2f7e909f60a471608a7912cb
icouldbreathe <ciomenas@icloud.com>
2022-05-11 13:40:33 +0100

    output ppm image

--- IOW_RayTracer/main.cpp
+++ IOW_RayTracer/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int imageWidth = 3;
+    const int imageHeight = 2;
+    const int maxColor = 255;
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n" << maxColor << "\n";
+    std::cout << "255 0 0 0 255 0 0 0 255\n255 255 0 255 255 255 0 0 0\n";
+
+    return 0;
 }
 
