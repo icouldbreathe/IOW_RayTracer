c9668d77da3c5d0459d0c48fa21fa4958fc8fb65
icouldbreathe <ciomenas@icloud.com>
2022-05-14 17:00:44 +0100

    add vec3 and color utility classes

--- IOW_RayTracer/main.cpp
+++ IOW_RayTracer/main.cpp
@@ -1,9 +1,12 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main()
 {
-    const int imageWidth = 512;
-    const int imageHeight = 512;
+    const int imageWidth = 1920;
+    const int imageHeight = 1080;
     const int maxColor = 255;
 
     std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n" << maxColor << "\n";
@@ -13,18 +16,10 @@ int main()
         std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
         for (int j = 0; j < imageWidth; ++j)
         {
-            // Scales the rgb values across the image
-            // Red from left to right 0-255, Green from top to bottom 255-0
-            // Blue tint of 0.25
-            auto r = double(j) / (imageWidth-1);
-            auto g = double(i) / (imageHeight-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+            // Red from left to right 0-255, Green from top to bottom 255-0, Blue tint of 0.25
+            color pixelColor(double(j)/(imageWidth-1), double(i)/(imageHeight-1), 0.25);
+            writeColor(std::cout, pixelColor);
+            std::cerr << pixelColor;
         }
     }
 
