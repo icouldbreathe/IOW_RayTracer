5c2539986d7e40ff2266598bd6b498fb5a41715c
icouldbreathe <ciomenas@icloud.com>
2022-06-03 00:24:37 +0100

    add camera

--- IOW_RayTracer/src/main.cpp
+++ IOW_RayTracer/src/main.cpp
@@ -1,13 +1,36 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
 
+color rayColor(const ray& r)
+{
+    vec3 unitDirection = unitVector(r.direction());
+    auto t = 0.5 * (unitDirection.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main()
 {
-    const int imageWidth = 1920;
-    const int imageHeight = 1080;
+    // Image
+    const auto aspectRatio = 16.0 / 9.0;
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+
     const int maxColor = 255;
+
+    // Camera
+    auto viewportHeight = 2.0;
+    auto viewportWidth = aspectRatio * viewportHeight;
+    auto focalLength = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewportWidth, 0, 0);
+    auto vertical = vec3(0, viewportHeight, 0);
+    auto lowerLeftCorner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focalLength);
+
+    // Render
 
     std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n" << maxColor << "\n";
     
@@ -16,14 +39,13 @@ int main()
         std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
         for (int j = 0; j < imageWidth; ++j)
         {
-            // Red from left to right 0-255, Green from top to bottom 255-0, Blue tint of 0.25
-            color pixelColor(double(j)/(imageWidth-1), double(i)/(imageHeight-1), 0.25);
+            auto u = double(j) / (imageWidth - 1);
+            auto v = double(i) / (imageHeight - 1);
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+            color pixelColor = rayColor(r);
             writeColor(std::cout, pixelColor);
         }
     }
 
     std::cerr << "\nDone.\n";
-    
-    return 0;
 }
-
