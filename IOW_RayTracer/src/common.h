229d690c18f2c14c8428c6650e6617998d29e052
Dovydas Ciomenas <ciomenas@icloud.com>
2022-06-21 22:04:55 +0100

    add random and clamp helper func

--- IOW_RayTracer/src/common.h
+++ IOW_RayTracer/src/common.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Common Headers
 
@@ -25,4 +26,20 @@ const double pi = 3.1415926535897932385;
 inline double degreesToRadians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double randomDouble()
+{
+    static std::uniform_int_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max)
+{
+    if (x < min)
+        return min;
+    if (x > max)
+        return max;
+    return x;
 }
