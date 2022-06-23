8fa23d3345a60c26bf995725b1209449f0e9c957
Dovydas Ciomenas <ciomenas@icloud.com>
2022-06-23 18:51:40 +0100

    add gamma correction

--- IOW_RayTracer/src/Color.h
+++ IOW_RayTracer/src/Color.h
@@ -11,10 +11,11 @@ void writeColor(std::ostream &out, Color pixelColor, int samplesPerPixel)
     auto b = pixelColor.z();
 
     // Divide color by the number of samples
+    // Gamma-correct for gamma=2.0
     auto scale = 1.0 / samplesPerPixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     out << static_cast<int>(255 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(255 * clamp(g, 0.0, 0.999)) << ' '
