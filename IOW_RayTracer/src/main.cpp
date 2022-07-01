3b8b2bc2224e9c551985303290ece654db3d11f2
Dovydas Ciomenas <ciomenas@icloud.com>
2022-07-01 16:57:11 +0100

    set up final scene

--- IOW_RayTracer/src/main.cpp
+++ IOW_RayTracer/src/main.cpp
@@ -9,6 +9,123 @@
 #include "Sphere.h"
 #include "common.h"
 
+HittableList testScene()
+{
+    HittableList world;
+
+    auto ground_material =
+        make_shared<LambertianDiffuseMaterial>(Color(0.5, 0.5, 0.5));
+    world.add(make_shared<Sphere>(Point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = randomDouble();
+            Point3 center(a + 0.9 * randomDouble(), 0.2,
+                          b + 0.9 * randomDouble());
+
+            if ((center - Point3(4, 0.2, 0)).length() > 0.9)
+            {
+                shared_ptr<Material> sphere_material;
+
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = Color::random() * Color::random();
+                    sphere_material =
+                        make_shared<LambertianDiffuseMaterial>(albedo);
+                    world.add(
+                        make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // MetalMaterial
+                    auto albedo = Color::random(0.5, 1);
+                    auto fuzz = randomDouble(0, 0.5);
+                    sphere_material = make_shared<MetalMaterial>(albedo, fuzz);
+                    world.add(
+                        make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+                else
+                {
+                    // glass
+                    sphere_material = make_shared<DielectricMaterial>(1.5);
+                    world.add(
+                        make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<DielectricMaterial>(1.5);
+    world.add(make_shared<Sphere>(Point3(0, 1, 0), 1.0, material1));
+
+    auto material2 =
+        make_shared<LambertianDiffuseMaterial>(Color(0.4, 0.2, 0.1));
+    world.add(make_shared<Sphere>(Point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<MetalMaterial>(Color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<Sphere>(Point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
+Color rayColor(const Ray &ray, const Hittable &world, int depth);
+
+int main()
+{
+    // Image
+    const auto aspectRatio = 3.0 / 2.0;
+    const int imageWidth = 1200;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
+    const int samplesPerPixel = 500;
+    const int maxColor = 255;
+    const int maxDepth = 50;
+
+    // World
+
+    auto world = testScene();
+
+    // Camera
+
+    Point3 lookFrom(13, 2, 3);
+    Point3 lookAt(0, 0, 0);
+    Vec3 vUp(0, 1, 0);
+    auto distanceToFocus = 10.0;
+    auto aperture = 0.1;
+    auto vfov = 20;
+
+    Camera camera(lookFrom, lookAt, vUp, vfov, aspectRatio, aperture,
+                  distanceToFocus);
+
+    // Render
+
+    std::cout << "P3\n"
+              << imageWidth << ' ' << imageHeight << "\n"
+              << maxColor << "\n";
+
+    for (int i = imageHeight - 1; i >= 0; --i)
+    {
+        std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
+        for (int j = 0; j < imageWidth; ++j)
+        {
+            Color pixelColor(0, 0, 0);
+            for (int s = 0; s < samplesPerPixel; ++s)
+            {
+                auto u = (j + randomDouble()) / (imageWidth - 1);
+                auto v = (i + randomDouble()) / (imageHeight - 1);
+
+                Ray ray = camera.getRay(u, v);
+                pixelColor += rayColor(ray, world, maxDepth);
+            }
+            writeColor(std::cout, pixelColor, samplesPerPixel);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
+
 Color rayColor(const Ray &ray, const Hittable &world, int depth)
 {
     HitRecord record;
@@ -36,63 +153,4 @@ Color rayColor(const Ray &ray, const Hittable &world, int depth)
     auto t = 0.5 * (unitDirection.y() + 1.0);
 
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
-}
-
-int main()
-{
-    // Image
-    const auto aspectRatio = 16.0 / 9.0;
-    const int imageWidth = 400;
-    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
-    const int samplesPerPixel = 100;
-    const int maxColor = 255;
-    const int maxDepth = 50;
-
-    // World
-    HittableList world;
-
-    auto materialGround =
-        make_shared<LambertianDiffuseMaterial>(Color(0.8, 0.8, 0.0));
-    auto materialCenter =
-        make_shared<LambertianDiffuseMaterial>(Color(0.1, 0.1, 0.5));
-    auto materialLeft = make_shared<DielectricMaterial>(1.5);
-    auto materialRight = make_shared<MetalMaterial>(Color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(
-        make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, materialGround));
-    world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, materialCenter));
-    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-    world.add(
-        make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.49, materialLeft));
-    world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, materialRight));
-
-    // Camera
-
-    Camera camera;
-
-    // Render
-
-    std::cout << "P3\n"
-              << imageWidth << ' ' << imageHeight << "\n"
-              << maxColor << "\n";
-
-    for (int i = imageHeight - 1; i >= 0; --i)
-    {
-        std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
-        for (int j = 0; j < imageWidth; ++j)
-        {
-            Color pixelColor(0, 0, 0);
-            for (int s = 0; s < samplesPerPixel; ++s)
-            {
-                auto u = (j + randomDouble()) / (imageWidth - 1);
-                auto v = (i + randomDouble()) / (imageHeight - 1);
-
-                Ray ray = camera.getRay(u, v);
-                pixelColor += rayColor(ray, world, maxDepth);
-            }
-            writeColor(std::cout, pixelColor, samplesPerPixel);
-        }
-    }
-
-    std::cerr << "\nDone.\n";
 }
